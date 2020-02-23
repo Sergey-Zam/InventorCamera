73ad8a84fd293f05255b20a57eb890bdfdc10568
Sergey Z <zamyatninss@gmail.com>
2020-02-23 17:29:17 +0300

    23.02.2020

--- InventorCamera/Form1.Designer.vb
+++ InventorCamera/Form1.Designer.vb
@@ -23,28 +23,147 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btnStart = New System.Windows.Forms.Button()
+        Me.tbSavePath = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnReadCoords = New System.Windows.Forms.Button()
+        Me.cbOnlyStandardViews = New System.Windows.Forms.CheckBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cbShowAllMessages = New System.Windows.Forms.CheckBox()
         Me.SuspendLayout()
         '
         'btnStart
         '
-        Me.btnStart.Location = New System.Drawing.Point(46, 12)
+        Me.btnStart.Location = New System.Drawing.Point(15, 102)
         Me.btnStart.Name = "btnStart"
-        Me.btnStart.Size = New System.Drawing.Size(187, 72)
+        Me.btnStart.Size = New System.Drawing.Size(369, 35)
         Me.btnStart.TabIndex = 0
-        Me.btnStart.Text = "Start"
+        Me.btnStart.Text = "Старт"
         Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'tbSavePath
+        '
+        Me.tbSavePath.Location = New System.Drawing.Point(15, 65)
+        Me.tbSavePath.Name = "tbSavePath"
+        Me.tbSavePath.ReadOnly = True
+        Me.tbSavePath.Size = New System.Drawing.Size(288, 20)
+        Me.tbSavePath.TabIndex = 1
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(309, 63)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 2
+        Me.btnBrowse.Text = "Обзор"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.SelectedPath = "C:\Users\Сергей\Desktop"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(283, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "1. Перед началом работы откройте сборку или деталь"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(259, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "2. Выберите директорию сохранения скриншотов"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 156)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(87, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Дополнительно"
+        '
+        'btnReadCoords
+        '
+        Me.btnReadCoords.Location = New System.Drawing.Point(15, 263)
+        Me.btnReadCoords.Name = "btnReadCoords"
+        Me.btnReadCoords.Size = New System.Drawing.Size(223, 23)
+        Me.btnReadCoords.TabIndex = 6
+        Me.btnReadCoords.Text = "Считать текущие координаты камеры"
+        Me.btnReadCoords.UseVisualStyleBackColor = True
+        '
+        'cbOnlyStandardViews
+        '
+        Me.cbOnlyStandardViews.AutoSize = True
+        Me.cbOnlyStandardViews.Location = New System.Drawing.Point(15, 181)
+        Me.cbOnlyStandardViews.Name = "cbOnlyStandardViews"
+        Me.cbOnlyStandardViews.Size = New System.Drawing.Size(248, 17)
+        Me.cbOnlyStandardViews.TabIndex = 7
+        Me.cbOnlyStandardViews.Text = "Делать снимки только стандартных* видов"
+        Me.cbOnlyStandardViews.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 201)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(363, 26)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "*Стандартные виды включают 6 граней и 4 изометрических вершины." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Всегда корректно" &
+    " отрабатывают для различных деталей/сборок."
+        '
+        'cbShowAllMessages
+        '
+        Me.cbShowAllMessages.AutoSize = True
+        Me.cbShowAllMessages.Location = New System.Drawing.Point(15, 240)
+        Me.cbShowAllMessages.Name = "cbShowAllMessages"
+        Me.cbShowAllMessages.Size = New System.Drawing.Size(331, 17)
+        Me.cbShowAllMessages.TabIndex = 9
+        Me.cbShowAllMessages.Text = "Показывать сообщение после каждого сделанного снимка"
+        Me.cbShowAllMessages.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(396, 298)
+        Me.Controls.Add(Me.cbShowAllMessages)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cbOnlyStandardViews)
+        Me.Controls.Add(Me.btnReadCoords)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.tbSavePath)
         Me.Controls.Add(Me.btnStart)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "InventorCamera"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents btnStart As Button
+    Friend WithEvents tbSavePath As TextBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnReadCoords As Button
+    Friend WithEvents cbOnlyStandardViews As CheckBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents cbShowAllMessages As CheckBox
 End Class
--- InventorCamera/Form1.vb
+++ InventorCamera/Form1.vb
@@ -27,9 +27,21 @@ Public Class Form1
             End Try
         End Try
 
+        'заполнить текущий путь сохранения файлов
+        tbSavePath.Text = FolderBrowserDialog1.SelectedPath
     End Sub
 
     Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        If _invApp.Documents.Count = 0 Then 'если не открыто ни 1 документа
+            MsgBox("В Inventor не открыто ни одного документа")
+            Return 'выход из функции обработчика кнопки
+        End If
+
+        If Not (_invApp.ActiveDocument.DocumentType = DocumentTypeEnum.kAssemblyDocumentObject Or _invApp.ActiveDocument.DocumentType = DocumentTypeEnum.kPartDocumentObject) Then
+            MsgBox("Требуется открыть документ сборки или детали")
+            Return
+        End If
+
         TestInventorCamera()
     End Sub
 
@@ -43,9 +55,8 @@ Public Class Form1
             Exit Sub
         End Try
 
-        'вид получен
-        'установить домашний вид
-        v.GoHome()
+        'вид получен 
+        v.GoHome() 'установить на домашний вид
 
         'установить тему с белым фоном
         _invApp.ColorSchemes.Item("Presentation").Activate() 'this color scheme contains a white background
@@ -54,78 +65,310 @@ Public Class Form1
         'инициализация камеры
         Dim camera As Camera = v.Camera 'camera.Eye.X or .Y or .Z ; camera.PerspectiveAngle - угол 
 
+        'изменение видов камеры и получение снимков
+        getStandardViews(v, camera) 'получить стандартно-доступные виды (все 6 граней и 4 Iso-вершины)
+        If (cbOnlyStandardViews.Checked = False) Then
+            getCustomViews(v, camera) 'получить кастомные виды (4 оставшихся Iso-вершины и 12 рёбер)
+        End If
+
+        MsgBox("Все снимки сделаны." & vbCrLf &
+               "Расположение на диске: " & FolderBrowserDialog1.SelectedPath)
+    End Sub
+
+    Private Sub getStandardViews(v As View, camera As Camera)
         'установить вид камеры: Front - спереди
         camera.ViewOrientationType = ViewOrientationTypeEnum.kFrontViewOrientation
         camera.Fit()
         camera.Apply() 'view update too
-        MakeScreen(v, "Front") 'сделать снимок экрана
+        MakeScreen(v, camera, "Front") 'сделать снимок экрана
 
         'установить вид камеры: Back - сзади
         camera.ViewOrientationType = ViewOrientationTypeEnum.kBackViewOrientation
         camera.Fit()
         camera.Apply() 'view update too
-        MakeScreen(v, "Back") 'сделать снимок экрана
+        MakeScreen(v, camera, "Back") 'сделать снимок экрана
 
         'установить вид камеры: Right - справа
         camera.ViewOrientationType = ViewOrientationTypeEnum.kRightViewOrientation
         camera.Fit()
         camera.Apply() 'view update too
-        MakeScreen(v, "Right") 'сделать снимок экрана
+        MakeScreen(v, camera, "Right") 'сделать снимок экрана
 
         'установить вид камеры: Left - слева
         camera.ViewOrientationType = ViewOrientationTypeEnum.kLeftViewOrientation
         camera.Fit()
         camera.Apply() 'view update too
-        MakeScreen(v, "Left") 'сделать снимок экрана
+        MakeScreen(v, camera, "Left") 'сделать снимок экрана
 
         'установить вид камеры: Top - сверху
         camera.ViewOrientationType = ViewOrientationTypeEnum.kTopViewOrientation
         camera.Fit()
         camera.Apply() 'view update too
-        MakeScreen(v, "Top") 'сделать снимок экрана
+        MakeScreen(v, camera, "Top") 'сделать снимок экрана
 
         'установить вид камеры: Bottom - снизу
         camera.ViewOrientationType = ViewOrientationTypeEnum.kBottomViewOrientation
         camera.Fit()
         camera.Apply() 'view update too
-        MakeScreen(v, "Bottom") 'сделать снимок экрана
+        MakeScreen(v, camera, "Bottom") 'сделать снимок экрана
 
         'установить вид камеры: IsoBottomLeft - снизу слева спереди
         camera.ViewOrientationType = ViewOrientationTypeEnum.kIsoBottomLeftViewOrientation
         camera.Fit()
         camera.Apply() 'view update too
-        MakeScreen(v, "IsoBottomLeft") 'сделать снимок экрана
+        MakeScreen(v, camera, "IsoBottomLeft") 'сделать снимок экрана
 
         'установить вид камеры: IsoBottomRight - снизу справа спереди
         camera.ViewOrientationType = ViewOrientationTypeEnum.kIsoBottomRightViewOrientation
         camera.Fit()
         camera.Apply() 'view update too
-        MakeScreen(v, "IsoBottomRight") 'сделать снимок экрана
+        MakeScreen(v, camera, "IsoBottomRight") 'сделать снимок экрана
 
         'установить вид камеры: IsoTopLeft - сверху слева спереди
         camera.ViewOrientationType = ViewOrientationTypeEnum.kIsoTopLeftViewOrientation
         camera.Fit()
         camera.Apply() 'view update too
-        MakeScreen(v, "IsoTopLeft") 'сделать снимок экрана
+        MakeScreen(v, camera, "IsoTopLeft") 'сделать снимок экрана
 
-        'установить вид камеры: IsoTopRight - сверху справа спереди
+        ''установить вид камеры: IsoTopRight - сверху справа спереди
         camera.ViewOrientationType = ViewOrientationTypeEnum.kIsoTopRightViewOrientation
         camera.Fit()
         camera.Apply() 'view update too
-        MakeScreen(v, "IsoTopRight") 'сделать снимок экрана
+        MakeScreen(v, camera, "IsoTopRight") 'сделать снимок экрана
     End Sub
 
-    Private Sub MakeScreen(v As View, description As String)
+    Private Sub getCustomViews(v As View, camera As Camera)
+        Dim oTG As TransientGeometry = _invApp.TransientGeometry 'инициализация TransientGeometry
+
+        ''установить кастомный вид камеры: IsoBottomLeftBack - снизу слева сзади
+        Dim oEye As Inventor.Point = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        Dim oTarget As Inventor.Point = oTG.CreatePoint(1, 1, 1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        Dim oUpVector As UnitVector = oTG.CreateUnitVector(1, 1, 0) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "IsoBottomLeftBack") 'сделать снимок экрана
+
+        'установить кастомный вид камеры: IsoBottomRightBack - снизу справа сзади
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(-1, 1, 1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 0) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "IsoBottomRightBack") 'сделать снимок экрана
+
+        'установить кастомный вид камеры: IsoTopLeftBack - сверху слева сзади
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(1, -1, 1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 0) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "IsoTopLeftBack") 'сделать снимок экрана
+
+        'установить кастомный вид камеры: IsoTopRightBack - сверху справа сзади
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(-1, -1, 1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 0) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "IsoTopRightBack") 'сделать снимок экрана
+
+        '--далее кастомные виды на ребра--
+        'установить вид на ребро: TopRight
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(-1, -1, 0) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "TopRightEdge") 'сделать снимок экрана
+
+        'установить вид на ребро: TopLeft
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(1, -1, 0) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "TopLeftEdge") 'сделать снимок экрана
+
+        'установить вид на ребро: BottomRight
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(-1, 1, 0) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "BottomRightEdge") 'сделать снимок экрана
+
+        'установить вид на ребро: BottomLeft
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(1, 1, 0) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "BottomLeftEdge") 'сделать снимок экрана
+
+        'установить вид на ребро: TopFront
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(0, -1, -1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "TopFrontEdge") 'сделать снимок экрана
+
+        'установить вид на ребро: TopBack
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(0, -1, 1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "TopBackEdge") 'сделать снимок экрана
+
+        'установить вид на ребро: BottomFront
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(0, 1, -1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "BottomFrontEdge") 'сделать снимок экрана
+
+        'установить вид на ребро: BottomBack
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(0, 1, 1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "BottomBackEdge") 'сделать снимок экрана
+
+        'установить вид на ребро: RightFront
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(-1, 0, -1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "RightFrontEdge") 'сделать снимок экрана
+
+        'установить вид на ребро: RightBack
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(-1, 0, 1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "RightBackEdge") 'сделать снимок экрана
+
+        'установить вид на ребро: LeftFront
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(1, 0, -1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "LeftFrontEdge") 'сделать снимок экрана
+
+        'установить вид на ребро: LeftBack
+        oEye = oTG.CreatePoint(0, 0, 0)
+        camera.Eye = oEye 'set camera Eye
+        oTarget = oTG.CreatePoint(1, 0, 1) 'Определяет положение целевой точки, которую наблюдатель просматривает на сцене (ось Z вида).
+        camera.Target = oTarget 'set camera Target
+        oUpVector = oTG.CreateUnitVector(1, 1, 1) 'Определяет вектор, определяющий, что «вверх» для наблюдателя.
+        camera.UpVector = oUpVector 'set camera UpVector
+        camera.Fit()
+        camera.Apply() 'apply camera settings
+        MakeScreen(v, camera, "LeftBackEdge") 'сделать снимок экрана
+    End Sub
+
+    Private Sub MakeScreen(v As View, camera As Camera, description As String)
         'Динамическое получение ширины и высоты вида. Стандарт: (1024, 768), но тогда возможны поетри части изображения
         Dim width = v.Width
         Dim height = v.Height
-        Dim desctopPath = My.Computer.FileSystem.SpecialDirectories.Desktop
+        Dim savePath = FolderBrowserDialog1.SelectedPath
         Dim name = description & " " & DateTime.Now.ToString("yyyy/MM/dd HH.mm.ss") & ".bmp"
-        v.SaveAsBitmap(desctopPath & "\" & name, width, height)
-        MsgBox("Screen is ready." & vbCrLf &
-               "Location: " & desctopPath & vbCrLf &
+        v.SaveAsBitmap(savePath & "\" & name, width, height)
+
+        If (cbShowAllMessages.Checked = True) Then
+            MsgBox("Screen is ready." & vbCrLf &
+               "Location: " & savePath & vbCrLf &
                "Name: " & name & vbCrLf &
                "Width(px): " & width & vbCrLf &
-               "Height(px): " & height)
+               "Height(px): " & height & vbCrLf &
+               "Camera" & vbCrLf &
+               "X: " & camera.Eye.X & vbCrLf &
+               "Y: " & camera.Eye.Y & vbCrLf &
+               "Z: " & camera.Eye.Z)
+        End If
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            tbSavePath.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub btnReadCoords_Click(sender As Object, e As EventArgs) Handles btnReadCoords.Click
+        If _invApp.Documents.Count = 0 Then 'если не открыто ни 1 документа
+            MsgBox("В Inventor не открыто ни одного документа")
+            Return 'выход из функции обработчика кнопки
+        End If
+
+        If Not (_invApp.ActiveDocument.DocumentType = DocumentTypeEnum.kAssemblyDocumentObject Or _invApp.ActiveDocument.DocumentType = DocumentTypeEnum.kPartDocumentObject) Then
+            MsgBox("Требуется открыть документ сборки или детали")
+            Return
+        End If
+
+        'попытка открыть вид (на экране должна быть открыта деталь или сборка)
+        Dim v As View
+        Try
+            v = _invApp.ActiveView
+        Catch ex As Exception
+            MsgBox("В Inventor должен быть открыт хотя бы один вид")
+            Exit Sub
+        End Try
+
+        Dim camera As Camera = v.Camera 'camera.Eye.X or .Y or .Z ; camera.PerspectiveAngle - угол 
+
+        MsgBox("X: " & camera.Eye.X & vbCrLf &
+               "Y: " & camera.Eye.Y & vbCrLf &
+               "Z: " & camera.Eye.Z)
     End Sub
 End Class
